deb47cc3135e9bde1166e62839c507483b8d1fd7
cyper <345343747@qq.com>
2017-04-03 22:44:55 +0800

    searchbox, FormControl, debounceTime, distinctUntilChanged.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {ModalController, NavController, Platform} from 'ionic-angular';
+import {Data} from "../../providers/data";
+import {Reddit} from "../../providers/reddit";
+import {Keyboard} from "@ionic-native/keyboard";
+import {FormControl} from "@angular/forms";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  subredditValue: string;
+  subredditControl: FormControl;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController,
+              public dataService: Data,
+              public redditService: Reddit,
+              public modalCtrl: ModalController,
+              public platform: Platform,
+              public keyboard: Keyboard) {
+    this.subredditControl = new FormControl();
   }
 
+  ionViewDidLoad(): void {
+    this.subredditControl.valueChanges.debounceTime(1500).distinctUntilChanged().subscribe(subreddit => {
+      if (subreddit != '' && subreddit) {
+        this.redditService.subreddit = subreddit;
+        this.changeSubreddit();
+        this.keyboard.close();
+      }
+    });
+    this.platform.ready().then(_ => {
+      this.loadSettings();
+    })
+  }
+
+  loadSettings(): void {
+    console.log('TODO: implement loadSettings()');
+  }
+
+
+  changeSubreddit() {
+
+  }
 }
