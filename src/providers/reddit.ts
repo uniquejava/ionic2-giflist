d3143feb6b92e138edddabf6ca04efd302bbac6a
cyper <345343747@qq.com>
2017-04-03 22:14:27 +0800

    home page layout and reddit provider attributes

--- src/providers/reddit.ts
+++ src/providers/reddit.ts
@@ -11,8 +11,43 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Reddit {
 
+  // the settings the user has supplied
+  settings: any;
+
+  // whether new GIFs are currently being fetched
+  loading: boolean = false;
+
+  // the entries for all the GIFs currently loaded
+  posts: any = [];
+
+  // the current subreddit
+  subreddit: string = 'gifs';
+
+  // the current page (i.e. how many times the user has clicked "Load More")
+  page: number = 1;
+
+  // the amount of GIFs to display per page
+  perPage: number = 15;
+
+  // A reference to the last post retrieved from Reddit (so we know where to start for the next page)
+  after: string;
+
+  // This will be used to store a reference to the length of the posts array
+  stopIndex: number;
+
+  // the sort order for GIFs
+  sort: string = 'hot';
+
+  // The app will keep trying to load more posts from reddit until it has enough for a full page of GIFs, moreCount is
+  // used to tell when it should stop trying to load more (i.e. if not enough GIFs are found after hitting the API 20 times
+  moreCount: number = 0;
+
   constructor(public http: Http) {
     console.log('Hello Reddit Provider');
+  }
+
+  fetchData(): void {
+
   }
 
 }
